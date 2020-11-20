b2694c475947bb45d8f4df05f4b1291cab203d08
HKnock <34215956+HKnock@users.noreply.github.com>
2020-11-20 15:35:02 +0300

    Tests

--- Bonch.py
+++ Bonch.py
@@ -2,23 +2,39 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-try:
-    browser = webdriver.Chrome()
-    browser.get("https://lk.sut.ru/cabinet/?login=no")
+browser = webdriver.Chrome()
+browser.get("https://lk.sut.ru/cabinet/?login=no")
 
-    browser.find_element_by_name("users").send_keys("lolu4ka@gmail.com")
-    browser.find_element_by_name("parole").send_keys("orifeh99")
+browser.find_element_by_name("users").send_keys("lolu4ka@gmail.com")
+browser.find_element_by_name("parole").send_keys("orifeh99")
 
-    browser.find_element_by_name("logButton").click()
+browser.find_element_by_name("logButton").click()
 
-    time.sleep(3)
+time.sleep(3)
 
-    browser.find_element_by_id("heading1").click()
-    browser.find_element_by_id("menu_li_6118").click()
+browser.find_element_by_id("heading1").click()
+browser.find_element_by_id("menu_li_6118").click()
 
-    time.sleep(3)
+time.sleep(3)
 
-    browser.find_element_by_link_text("Начать занятие").click()
+#browser.find_element_by_link_text("Начать занятие").click()
 
-except:
-    print("Препод не ожил к паре(9(\nИли произошла внутренняя ошибка в программе")
+time.sleep(5)
+
+browser.get("https://lk.sut.ru/cabinet/?login=no")
+
+browser.find_element_by_name("users").send_keys("gnataly2106@mail.ru")
+browser.find_element_by_name("parole").send_keys("Andrey2002")
+
+browser.find_element_by_name("logButton").click()
+
+time.sleep(3)
+
+browser.find_element_by_id("heading1").click()
+browser.find_element_by_id("menu_li_6118").click()
+
+time.sleep(3)
+
+#browser.find_element_by_link_text("Начать занятие").click()
+
+#print("Препод не ожил к паре(9(\nИли произошла внутренняя ошибка в программе")
