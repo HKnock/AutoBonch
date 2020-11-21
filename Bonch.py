8e5df0074fac2403191c7745bf3c91bf9c04394c
HKnock <34215956+HKnock@users.noreply.github.com>
2020-11-21 09:12:56 +0300

    Новый релиз

--- Bonch.py
+++ Bonch.py
@@ -18,86 +18,103 @@ try:
     print("Программа берёт аДский разгон на балдёж")
 
 #Первый пользователь
+    try:
+        print("Программа балдеет за первого пользователя")
 
-    browser.find_element_by_name("users").send_keys("lolu4ka@gmail.com")
-    browser.find_element_by_name("parole").send_keys("orifeh99")
+        browser.find_element_by_name("users").send_keys("lolu4ka@gmail.com")
+        browser.find_element_by_name("parole").send_keys("orifeh99")
 
-    browser.find_element_by_name("logButton").click()
+        browser.find_element_by_name("logButton").click()
 
-    time.sleep(3)
+        time.sleep(3)
 
-    browser.find_element_by_id("heading1").click()
-    browser.find_element_by_id("menu_li_6118").click()
+        browser.find_element_by_id("heading1").click()
+        browser.find_element_by_id("menu_li_6118").click()
 
-    time.sleep(3)
+        time.sleep(3)
 
-    browser.find_element_by_link_text("Начать занятие").click()
+        browser.find_element_by_link_text("Начать занятие").click()
 
-    time.sleep(5)
+        time.sleep(5)
+    except:
+        print("Пользователь сломал систему(9(\nПрограмма не балдеет")
 
 #Второй пользователь
+    try:
+        print("Программа балдеет за второго пользователя")
 
-    browser.get("https://lk.sut.ru/cabinet/?login=no")
+        browser.get("https://lk.sut.ru/cabinet/?login=no")
 
-    browser.find_element_by_name("users").send_keys("gnataly2106@mail.ru")
-    browser.find_element_by_name("parole").send_keys("Andrey2002")
+        browser.find_element_by_name("users").send_keys("gnataly2106@mail.ru")
+        browser.find_element_by_name("parole").send_keys("Andrey2002")
 
-    browser.find_element_by_name("logButton").click()
+        browser.find_element_by_name("logButton").click()
 
-    time.sleep(3)
+        time.sleep(3)
 
-    browser.find_element_by_id("heading1").click()
-    browser.find_element_by_id("menu_li_6118").click()
+        browser.find_element_by_id("heading1").click()
+        browser.find_element_by_id("menu_li_6118").click()
 
-    time.sleep(3)
+        time.sleep(3)
 
-    print("Программа балдеет")
+        print("Программа балдеет")
 
-    browser.find_element_by_link_text("Начать занятие").click()
+        browser.find_element_by_link_text("Начать занятие").click()
 
-    time.sleep(5)
+        time.sleep(5)
+    except:
+        print("Пользователь сломал систему(9(\nПрограмма не балдеет")
 
 #Третий пользователь
+    try:
+        print("Программа балдеет за третьего пользователя")
 
-    browser.get("https://lk.sut.ru/cabinet/?login=no")
+        browser.get("https://lk.sut.ru/cabinet/?login=no")
 
-    browser.find_element_by_name("users").send_keys("ivanoov16@mail.ru")
-    browser.find_element_by_name("parole").send_keys("novgorod03")
+        browser.find_element_by_name("users").send_keys("ivanoov16@mail.ru")
+        browser.find_element_by_name("parole").send_keys("novgorod03")
 
-    browser.find_element_by_name("logButton").click()
+        browser.find_element_by_name("logButton").click()
 
-    time.sleep(3)
+        time.sleep(3)
 
-    browser.find_element_by_id("heading1").click()
-    browser.find_element_by_id("menu_li_6118").click()
+        browser.find_element_by_id("heading1").click()
+        browser.find_element_by_id("menu_li_6118").click()
 
-    time.sleep(3)
+        time.sleep(3)
 
-    print("Программа балдеет")
+        print("Программа балдеет")
 
-    browser.find_element_by_link_text("Начать занятие").click()
+        browser.find_element_by_link_text("Начать занятие").click()
 
-    time.sleep(3)
+        time.sleep(3)
+    except:
+        print("Пользователь сломал систему(9(\nПрограмма не балдеет")
 
 #Четвёртый пользователь
+    try:
+        print("Программа балдеет за третьего пользователя")
 
-    browser.get("https://lk.sut.ru/cabinet/?login=no")
+        browser.get("https://lk.sut.ru/cabinet/?login=no")
 
-    browser.find_element_by_name("users").send_keys("lyagiceva@mail.ru")
-    browser.find_element_by_name("parole").send_keys("Kirik5218")
+        browser.find_element_by_name("users").send_keys("lyagiceva@mail.ru")
+        browser.find_element_by_name("parole").send_keys("Kirik5218")
 
-    browser.find_element_by_name("logButton").click()
+        browser.find_element_by_name("logButton").click()
 
-    time.sleep(3)
+        time.sleep(3)
 
-    browser.find_element_by_id("heading1").click()
-    browser.find_element_by_id("menu_li_6118").click()
+        browser.find_element_by_id("heading1").click()
+        browser.find_element_by_id("menu_li_6118").click()
 
-    time.sleep(3)
+        time.sleep(3)
 
-    print("Программа балдеет")
+        print("Программа балдеет")
 
-    browser.find_element_by_link_text("Начать занятие").click()
+        browser.find_element_by_link_text("Начать занятие").click()
+
+    except:
+        print("Пользователь сломал систему(9(\nПрограмма не балдеет")
 
 except:
     print("Препод не ожил к паре(9(\nИли произошла внутренняя ошибка в программе\nПрограмма не балдеет(9(")
